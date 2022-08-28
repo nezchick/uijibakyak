258769ee5ca9a9716ae9b7b91960be6b71cb5f44
nezchick <245nha@gmail.com>
2022-08-28 18:32:53 +0900

    01

---
+++ src/main/java/com/seung/uijibakyak/UijibakyakApplication.java
@@ -0,0 +1,13 @@
+package com.seung.uijibakyak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UijibakyakApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UijibakyakApplication.class, args);
+  }
+
+}
