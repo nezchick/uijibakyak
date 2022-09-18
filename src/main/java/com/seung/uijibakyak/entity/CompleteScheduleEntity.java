de60904c669e6b8100b07ea8f2e7359cc5afb2cc
SEUNG <245nha@gmail.com>
2022-09-19 00:25:27 +0900

    load schedule and create schedule

--- src/main/java/com/seung/uijibakyak/entity/CompleteScheduleEntity.java
+++ src/main/java/com/seung/uijibakyak/entity/CompleteScheduleEntity.java
@@ -1,22 +1,13 @@
 package com.seung.uijibakyak.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDate;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@Data
-@Entity(name = "schedule")
-public class ScheduleEntity {
+@Entity(name = "complete_schedule")
+public class CompleteScheduleEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -24,8 +15,6 @@ public class ScheduleEntity {
     private String schedule;
     //아이디
     private String userId;
-    //입력시간
-    private LocalDate inputTime;
-    //일정기한
-    private LocalDate deadline;
+    //완료 시점
+    private LocalDate completeDate;
 }
--- src/main/java/com/seung/uijibakyak/repository/ScheduleRepository.java
+++ src/main/java/com/seung/uijibakyak/repository/ScheduleRepository.java
@@ -1,7 +1,13 @@
 package com.seung.uijibakyak.repository;
 
 import com.seung.uijibakyak.entity.ScheduleEntity;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> {
+    List<ScheduleEntity> findAllByDeadlineIsNull(Sort sort);
+    List<ScheduleEntity> findAllByDeadlineIsNotNull(Sort sort);
 }
